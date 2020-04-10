19dde6d7ec6da11c449b8a11238bff4c81cb1627
Priyesh Padmavilasom <ppadmavilasom@vmware.com>
2020-04-10 01:03:19 +0000

    check all versions

--- ci/repo_check.sh
+++ ci/repo_check.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 echo $RELEASEVER
 
+if [ ! -f '/usr/bin/sed' ]
+then
+  tdnf install sed -y
+fi
+
 pushd /etc/yum.repos.d
 for repo in $(ls *.repo)
 do
@@ -9,6 +14,16 @@ done
 popd
 
 cat /etc/yum.repos.d/*.repo | grep baseurl
+
+enabled_repos=`cat /etc/yum.repos.d/*.repo | grep enabled=1 | wc -l`
+tdnf repolist
+repolist_result_enabled=`tdnf repolist | grep enabled | wc -l`
+
+if [ $repolist_result_enabled -ne $enabled_repos ]
+  then
+    echo "Expected $enabled_repos enabled repos in repolist. Found $repolist_result_enabled"
+    exit 1
+fi
 
 for f in $(cat updates/$RELEASEVER)
 do
